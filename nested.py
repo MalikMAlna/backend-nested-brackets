bad62b7a938cc96578541f4633cbfae094bd8b4b
Mike A <jimalik2525@gmail.com>
2020-02-28 16:47:18 -0500

    Finished, now gonna experiment with it before submission

--- nested.py
+++ nested.py
@@ -3,20 +3,49 @@
 """
 Module docstring: One line description of what your program does.
 """
-__author__ = "???"
+__author__ = """Watch Demo of Nested Brackets
+                with Walkthrough by madarp,
+                and valid parenthesis"""
 
 import sys
+openers = ["(*", "(", "[", "{", "<"]
+closers = ["*)", ")", "]", "}", ">"]
 
 
 def is_nested(line):
-    """Validate a single input line for correct nesting"""
-    pass
+    stack = []
+    count = 0
+    while line:
+        token = line[0]
+        if line.startswith("(*"):
+            token = "(*"
+        elif line.startswith("*)"):
+            token = "*)"
+        count += 1
+        line = line[len(token):]
+        if token in openers:
+            stack.append(token)
+        elif token in closers:
+            closer_index = closers.index(token)
+            expected_opener = openers[closer_index]
+            if stack.pop() != expected_opener:
+                return "NO " + str(count)
+    if stack:
+        return "NO " + str(count)
+
+    return "YES"
 
 
 def main(args):
     """Open the input file and call `is_nested()` for each line"""
     # Results: print to console and also write to output file
-    pass
+    print("Testing for the Nesting: {}".format(args[0]))
+    with open(args[0]) as ifile:
+        with open("output.txt", "w") as ofile:
+            for line in ifile:
+                result_str = is_nested(line)
+                print(result_str)
+                ofile.write(result_str + "\n")
 
 
 if __name__ == '__main__':
